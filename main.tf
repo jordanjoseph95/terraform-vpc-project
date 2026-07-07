ebaaa344a21c11107e8d2d080df416c8d61f2033
Jordan Joseph <jjoseph1608@outlook.com>
2026-07-07 05:13:38 +0100

    Add internet gateway and public route table

--- main.tf
+++ main.tf
@@ -42,3 +42,34 @@ resource "aws_subnet" "public_b" {
     Name = "jordan-public-subnet-b"
   }
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "jordan-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "jordan-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
